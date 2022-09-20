95ab0023a6c95faf43ca94909598c6d83418baf5
ksfatih <90271453+ksfatih@users.noreply.github.com>
2022-09-20 13:07:04 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = 'https://eu-de.functions.appdomain.cloud/api/v1/web/47e6f3b6-3bcb-44a0-8374-c7a670959e78/guestbook/read-guestbook-entries-sequence';
+const apiUrl = 'https://eu-de.functions.appdomain.cloud/api/v1/web/47e6f3b6-3bcb-44a0-8374-c7a670959e78/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence.json`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
